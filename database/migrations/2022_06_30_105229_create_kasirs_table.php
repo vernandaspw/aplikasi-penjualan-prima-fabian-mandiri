938e416f14e733b2b9a41e10103bc95a3633fa39
“vernandaspw” <vernandaspw@gmail.com>
2022-07-05 15:32:58 +0700

    bersih

--- database/migrations/2022_06_30_105229_create_kasirs_table.php
+++ database/migrations/2022_06_30_105229_create_kasirs_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('kasirs', function (Blueprint $table) {
             $table->id();
+            // $table
             $table->timestamps();
         });
     }
