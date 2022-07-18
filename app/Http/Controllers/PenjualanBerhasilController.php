f4130e31bb302f24b1069e946b4ed56d02647e37
“vernandaspw” <vernandaspw@gmail.com>
2022-07-18 16:23:53 +0700

    dddd

--- app/Http/Controllers/PenjualanBerhasilController.php
+++ app/Http/Controllers/PenjualanBerhasilController.php
@@ -20,18 +20,18 @@ class PenjualanBerhasilController extends Controller
     {
         $data = Transaksi::with('transaksiitem', 'pegawai', 'konsumen', 'metodekirim', 'metodepembayaran')->where('no_transaksi', $id)->first();
         $pdf = Pdf::loadView('Exports.struk', compact('data'))->setPaper('a4', 'portrait');
-        return $pdf->stream();
+        return $pdf->stream('struk-'.$id);
     }
     public function cetaknota($id)
     {
         $data = Transaksi::with('transaksiitem', 'pegawai', 'konsumen', 'metodekirim', 'metodepembayaran')->where('no_transaksi', $id)->first();
         $pdf = Pdf::loadView('Exports.nota', compact('data'))->setPaper('a4', 'portrait');
-        return $pdf->stream();
+        return $pdf->stream('nota-'.$id);
     }
     public function cetaksuratjalan($id)
     {
         $data = Transaksi::with('transaksiitem', 'pegawai', 'konsumen', 'metodekirim', 'metodepembayaran')->where('no_transaksi', $id)->first();
         $pdf = Pdf::loadView('Exports.surat-jalan', compact('data'))->setPaper('a4', 'portrait');
-        return $pdf->stream('surat-jalan');
+        return $pdf->stream('surat-jalan-'.$id);
     }
 }
