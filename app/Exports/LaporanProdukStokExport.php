1e186590e6c166273bd762333953da134353bd34
“vernandaspw” <vernandaspw@gmail.com>
2022-07-21 21:54:55 +0700

    dd

--- app/Exports/LaporanProdukStokExport.php
+++ app/Exports/LaporanProdukStokExport.php
@@ -3,15 +3,27 @@
 namespace App\Exports;
 
 use App\Models\Produk;
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromCollection;
 
-class LaporanProdukStokExport implements FromCollection
+class LaporanProdukStokExport implements FromView
 {
     /**
     * @return \Illuminate\Support\Collection
     */
-    public function collection()
+    use Exportable;
+
+    public function __construct($data)
     {
-        return Produk::all();
+        $this->data = $data;
+    }
+
+    public function view(): View
+    {
+        return view('Exports.laporan-produk-stok', [
+            'datas' => $this->data
+        ]);
     }
 }
